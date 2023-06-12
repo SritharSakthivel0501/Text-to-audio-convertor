7a97bd76ae13110533989951519d87ee657fd945
srithardckap <sritharsdckap.com>
2023-06-12 15:39:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const speak = document.querySelector(".speakBtn");
+
+const textarea = document.querySelector(".textarea");
+
+speak.addEventListener("click",()=>{
+    let utterance = new SpeechSynthesisUtterance(textarea.value);
+    speechSynthesis.speak(utterance);
+});
